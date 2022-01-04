de1f126ed82a1563d993693f56b33d1b087843fe
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2022-01-04 18:09:50 -0300

    feat: external links

--- src/links.ts
+++ src/links.ts
@@ -1,1 +1,10 @@
-export const links = [];
+export const links = [
+  'https://vimeo.com/tennisproian/review/265448536/88fcb9c1a3',
+  'https://vimeo.com/tennisproian/review/265449630/1983a6159a',
+  'https://vimeo.com/tennisproian/review/265451095/3c43b3362c',
+  'https://vimeo.com/tennisproian/review/265452563/9b788ae9d6',
+  'https://vimeo.com/tennisproian/review/265453622/f16c4c939e',
+  'https://vimeo.com/tennisproian/review/265453863/a79adec16c',
+  'https://vimeo.com/tennisproian/review/265454266/954d28c893',
+  'https://vimeo.com/tennisproian/review/265456007/380d64a911',
+];
